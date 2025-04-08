585610d9bd721b04d07a7da0389434ede1081dfc
Aleksey Deryugin <derugin@oborot.ru>
2025-04-08 21:39:07 +0300

    cleaning

--- app/app.vue
+++ app/app.vue
@@ -5,16 +5,10 @@
 
 <!--    <NuxtLayout />
     -->
-
     <div class="container-fluid h-100">
         <div class="row h-100">
             <div class="col-md-2 h-100">
                 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light h-100">
-<!--                    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-                        <span class="fs-4">Track</span>
-                    </a>
-                    <hr>-->
                     <div class="dropdown">
                         <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
@@ -68,43 +62,4 @@ html, body, #__nuxt {
     height: 100%;
 }
 
-/*html {
-    font-family: "Montserrat", serif;
-    font-optical-sizing: auto;
-    font-weight: 400;
-    font-style: normal;
-}*/
-
-/*html, body, #__nuxt {
-    height: 100%;
-}
-
-html > body {
-    font-family: Inter;
-}
-
-::-webkit-scrollbar {
-    width: 6px;
-    height: 6px;
-}
-
-::-webkit-scrollbar-track {
-    background: rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-}
-
-::-webkit-scrollbar-thumb {
-    background: rgba(0, 0, 0, 0.2);
-    border-radius: 4px;
-    transition: background 0.3s;
-}
-
-::-webkit-scrollbar-thumb:hover {
-    background: rgba(0, 0, 0, 0.3);
-}
-
-* {
-    scrollbar-width: thin;
-    scrollbar-color: rgba(0, 0, 0, 0.2) rgba(0, 0, 0, 0.05);
-}*/
 </style>
