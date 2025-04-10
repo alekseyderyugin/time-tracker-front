e1d2ab99263a8d8c57032e74ecb90d7261458e0b
Aleksey Deryugin <derugin@oborot.ru>
2025-04-10 17:19:55 +0300

    time-tracker-front-6 | поправил картинук

--- app/app.vue
+++ app/app.vue
@@ -11,7 +11,7 @@
                 <div class="d-flex flex-column flex-shrink-0 p-3 bg-light h-100">
                     <div class="dropdown">
                         <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                            <img src="/images/user.jpeg" alt="" width="32" height="32" class="rounded-circle me-2">
                             <strong>Aleksey Deryugin</strong>
                         </a>
                         <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
